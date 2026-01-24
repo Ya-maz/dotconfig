82c835e09afbf12a822d3b73d90120e1898d8d94
Almaz Yakhin <yamaz.job@gmail.com>
2026-01-24 15:08:46 +0300

    vifm config update with nvim_ultra_weak

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 -- code
 require("yamaz.core")
 require("yamaz.lazy")
+
 -- langmapper break next keymap:
 -- ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 -- ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -16,3 +17,14 @@ require("yamaz.lazy")
 -- use font
 -- Hack Nerd Font
 -- https://www.nerdfonts.com/font-downloads
+--
+local info = debug.getinfo(1, "S")
+local config_file = info.source:sub(2)  -- убираем '@'
+
+-- Динамическая версия Neovim
+local v = vim.version()
+local version_str = string.format("%d.%d.%d", v.major, v.minor, v.patch)
+
+-- Если нужно, можно и в :messages
+print(string.format("[NVIM-CONFIG]: %s", config_file))
+print(string.format("[NVIM-VERSION]: %s", version_str))
--- nvim/lua/yamaz/lazy.lua
+++ nvim/lua/yamaz/lazy.lua
@@ -16,6 +16,14 @@ vim.opt.rtp:prepend(lazypath)
 -- require('lazy').setup({
 -- {'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim'}}
 -- })
+--
+if utils.is_ultra_weak_mode() then
+    print("Ultra weak mode enabled! (core only and oil plugin)")
+    require("lazy").setup({
+        { import = "yamaz.plugins.oil" },
+    })
+    return
+end
 
 if utils.is_weak_mode() then
     -- Легкая конфигурация
@@ -27,13 +35,14 @@ if utils.is_weak_mode() then
     require("lazy").setup({
         { import = "yamaz.plugins" },
     })
-else
-    -- Обычная конфигурация
-    -- Добавлен alias
-    -- alias n='nvim'
-    print("Normal mode enabled!")
-    require("lazy").setup({
-        { import = "yamaz.plugins" },
-        { import = "yamaz.plugins.lsp" }
-    })
+    return
 end
+
+-- Обычная конфигурация
+-- Добавлен alias
+-- alias n='nvim'
+print("Normal mode enabled!")
+require("lazy").setup({
+    { import = "yamaz.plugins" },
+    { import = "yamaz.plugins.lsp" }
+})
