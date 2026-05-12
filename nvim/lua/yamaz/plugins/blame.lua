60ef978a89192d036f684cb6369b0684e203085f
Almaz Yakhin <yamaz.job@gmail.com>
2026-05-12 14:03:15 +0300

    update nvim

--- nvim/lua/yamaz/plugins/blame.lua
+++ nvim/lua/yamaz/plugins/blame.lua
@@ -1,9 +1,8 @@
 return {
-    {
-        "f-person/git-blame.nvim",
-        config = function()
-            vim.keymap.set("n", "<leader>bb", "<cmd>GitBlameToggle<cr>",
-            { silent = true, noremap = true })
-        end,
-    }
+	{
+		"f-person/git-blame.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>gb", "<cmd>GitBlameToggle<cr>", { silent = true, noremap = true })
+		end,
+	},
 }
